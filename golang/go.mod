d0f641e3baefcbd1788d7f235285fd07e2b0db9e
wanz <wawan.randianto@quipper.com>
2022-11-08 16:17:32 +0700

    pusher channel auth

--- golang/go.mod
+++ golang/go.mod
@@ -2,9 +2,15 @@ module github.com/wawanrandianto1/beampusher
 
 go 1.19
 
+require github.com/go-chi/render v1.0.2
+
 require (
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pusher/push-notifications-go v0.0.0-20200210154345-764224c311b8 // indirect
+	github.com/pusher/pusher-http-go/v5 v5.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
 )
--- golang/main.go
+++ golang/main.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pusher/pusher-http-go/v5"
 )
 
-const (
-	instanceId = "dc1dfd43-1815-4b60-9ea0-b59937f51dd7"
-	secretKey  = "abcde"
-)
+// const (
+// 	instanceId = "dc1dfd43-1815-4b60-9ea0-b59937f51dd7"
+// 	secretKey  = "abcde"
+// )
 
-type Notif struct {
-}
+// type Notif struct {
+// }
 
-func (n *Notif) publishToInterest() (error, map[string]interface{}) {
-	url := "https://" + instanceId + ".pushnotifications.pusher.com/publish_api/v1/instances/" + instanceId + "/publishes/interests"
-	body := []byte(`{
-		"title": "Post title",
-		"body": "Post description",
-		"userId": 1
-	}`)
+// func (n *Notif) publishToInterest() (error, map[string]interface{}) {
+// 	url := "https://" + instanceId + ".pushnotifications.pusher.com/publish_api/v1/instances/" + instanceId + "/publishes/interests"
+// 	body := []byte(`{
+// 		"title": "Post title",
+// 		"body": "Post description",
+// 		"userId": 1
+// 	}`)
 
-	resp, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		return err, map[string]interface{}{}
-	}
+// 	resp, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+// 	if err != nil {
+// 		return err, map[string]interface{}{}
+// 	}
 
-	var res map[string]interface{}
-	json.NewDecoder(resp.Body).Decode(&res)
-	return nil, res
+// 	var res map[string]interface{}
+// 	json.NewDecoder(resp.Body).Decode(&res)
+// 	return nil, res
+// }
+
+type Respon struct {
+	Message string `json:"message"`
 }
 
 func main() {
@@ -43,5 +49,42 @@ func main() {
 		w.Write([]byte("welcome"))
 	})
 
-	http.ListenAndServe(":3001", r)
+	r.Post("/pusher/auth", func(w http.ResponseWriter, r *http.Request) {
+		pusherClient := pusher.Client{
+			AppID:   "1494118",
+			Key:     "e48e401e9da913009f95",
+			Secret:  "8bf1185ce7e6c74b65dd",
+			Cluster: "ap1",
+		}
+
+		log.Printf("received method: %s", r.Method)
+		params, _ := ioutil.ReadAll(r.Body) // expects params to contain socket_id & channel_name
+
+		log.Printf("params: %v", string(params))
+
+		// userData := map[string]interface{}{"id": "1234"} // get user id from auth service
+		response, err := pusherClient.AuthorizePrivateChannel(params)
+		if err != nil {
+			log.Printf("error pusher: %v", err)
+			http.Error(w, http.StatusText(500), 500)
+			w.Write([]byte("auth private channel :" + err.Error()))
+			return
+		}
+
+		log.Printf("response: %s", response)
+
+		responseOk, err := json.Marshal(Respon{"OK"})
+		if err != nil {
+			log.Printf("error: %v", err)
+			http.Error(w, http.StatusText(500), 500)
+			w.Write([]byte("json convert : " + err.Error()))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseOk)
+	})
+
+	http.ListenAndServe(":5000", r)
 }
